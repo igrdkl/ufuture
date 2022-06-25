0d78e5875feb4d8f4a2c437d1d8fdeb88b7dd3a1
IgorDikal <igrdkl@icloud.com>
2022-06-25 18:52:37 +0300

    first commit

---
+++ docs/js/scripts.js
@@ -0,0 +1,2 @@
+
+const swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-next",prevEl:".swiper-prev"},scrollbar:{el:".swiper-scrollbar"}});
